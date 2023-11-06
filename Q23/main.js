fd05e44b8483092eff05f37c08851eb1e6a0eb53
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:34:02 +0500

    Add files via upload

---
+++ Q23/main.js
@@ -0,0 +1,23 @@
+var number = 7;
+console.log("Is number equal to 7? I predict True.");
+console.log(number === 7); // True
+console.log("Is number not equal to 5? I predict True.");
+console.log(number !== 5); // True
+console.log("Is number greater than 3? I predict True.");
+console.log(number > 3); // True
+console.log("Is number less than or equal to 7? I predict True.");
+console.log(number <= 7); // True
+console.log("Is 'apple' not equal to 'banana'? I predict True.");
+console.log('apple' !== "apple"); // True
+var x = 10;
+var y = 5;
+console.log("Is x + y equal to 15? I predict True.");
+console.log(x + y === 15); // True
+console.log("Is x greater than y? I predict True.");
+console.log(x > y); // True
+console.log("Is x less than 5 and y less than 10? I predict True.");
+console.log(x < 5 && y < 10); // True
+console.log("Is x less than 5 or y less than 3? I predict True.");
+console.log(x < 5 || y < 3); // True
+console.log("Is x less than 5 and y greater than 3? I predict False.");
+console.log(x < 5 && y > 3); // False
