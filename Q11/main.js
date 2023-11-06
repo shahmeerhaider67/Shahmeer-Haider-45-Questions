d3334df82672d044fe79208ed56d403e011884c7
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:12:42 +0500

    Add files via upload

---
+++ Q11/main.js
@@ -0,0 +1,7 @@
+// Define an array of friends' names
+var names = ["Shahmeer", "Wayne", "Shelby", "Wick"];
+// Print each person's name one at a time
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log(name_1);
+}
