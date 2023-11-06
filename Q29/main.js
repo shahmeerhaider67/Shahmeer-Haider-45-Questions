ffac9749be587dd9ec19e4686eba23bb807ba01f
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:41:40 +0500

    Add files via upload

---
+++ Q29/main.js
@@ -0,0 +1,16 @@
+var favoriteFruits = ["apple", "banana", "strawberry"];
+if (favoriteFruits.includes("apple")) {
+    console.log("You really like apples!");
+}
+if (favoriteFruits.includes("banana")) {
+    console.log("You really like bananas!");
+}
+if (favoriteFruits.includes("strawberry")) {
+    console.log("You really like strawberries!");
+}
+if (favoriteFruits.includes("orange")) {
+    console.log("You really like oranges!");
+}
+if (favoriteFruits.includes("grape")) {
+    console.log("You really like grapes!");
+}
