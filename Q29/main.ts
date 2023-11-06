447dbfd3a9fea9f268e724e4b9f16b6bb2faca75
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:41:20 +0500

    Create main.ts

---
+++ Q29/main.ts
@@ -0,0 +1,22 @@
+//#Q29
+let favoriteFruits = ["apple", "banana", "strawberry"];
+
+if (favoriteFruits.includes("apple")) {
+  console.log("You really like apples!");
+} 
+
+if (favoriteFruits.includes("banana")) {
+  console.log("You really like bananas!");
+}
+
+if (favoriteFruits.includes("strawberry")) {
+  console.log("You really like strawberries!");
+}
+
+if (favoriteFruits.includes("orange")) {
+  console.log("You really like oranges!");
+}
+
+if (favoriteFruits.includes("grape")) {
+  console.log("You really like grapes!");
+}
