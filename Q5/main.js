b80281a7005826b76de90e16a0622515c47c27db
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 19:28:08 +0500

    Add files via upload

---
+++ Q5/main.js
@@ -0,0 +1,4 @@
+var famous_person = "Albert Einstein";
+var quote = "Imagination is more important than knowledge.";
+var message = "\"".concat(quote, "\"\n- ").concat(famous_person);
+console.log(message);
