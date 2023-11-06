b73c3a793ce092b5acf9447c1b36549cf5ba9c0a
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:14:35 +0500

    Add files via upload

---
+++ Q12/main.js
@@ -0,0 +1,7 @@
+// Define an array of friends' names
+var names = ["Batman", "Superman", "Thor", "IronMan"];
+// Print a personalized message to each person
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("Hello ".concat(name_1, ", I hope you're doing well!"));
+}
