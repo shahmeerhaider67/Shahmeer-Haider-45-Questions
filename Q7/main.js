6cf0d901c00caafc2bc3bca88b2704a4ac50f23e
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 19:32:58 +0500

    Add files via upload

---
+++ Q7/main.js
@@ -0,0 +1,12 @@
+// Addition operation
+var additionResult = 5 + 3;
+console.log("Addition Result: ".concat(additionResult));
+// Subtraction operation
+var subtractionResult = 12 - 4;
+console.log("Subtraction Result: ".concat(subtractionResult));
+// Multiplication operation
+var multiplicationResult = 2 * 4;
+console.log("Multiplication Result: ".concat(multiplicationResult));
+// Division operation
+var divisionResult = 64 / 8;
+console.log("Division Result: ".concat(divisionResult));
