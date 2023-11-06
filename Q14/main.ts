5fd0f159c794c8968981ebad209c35c141d417b9
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:16:50 +0500

    Create main.ts

---
+++ Q14/main.ts
@@ -0,0 +1,8 @@
+//#Q14
+// Define a list of people to invite to dinner
+const invitees: string[] = ["Christian Bale", "Tom Cruise", "Dwayne Johnson"];
+
+// Print invitations to each person
+for (const invitee of invitees) {
+    console.log(`Dear ${invitee}, I Would like to invite you to a dinner event. Please let me know if you can make it.`);
+}
