83f31f575529735fc59c629a3c74685039340b57
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:15:46 +0500

    Add files via upload

---
+++ Q13/main.js
@@ -0,0 +1,7 @@
+// Define a list of modes of transportation
+var transportationModes = ["Car", "Motorcycle", "Bicycle", "Scooter"];
+// Print statements about these modes of transportation
+for (var _i = 0, transportationModes_1 = transportationModes; _i < transportationModes_1.length; _i++) {
+    var mode = transportationModes_1[_i];
+    console.log("I would like to own a ".concat(mode, "."));
+}
