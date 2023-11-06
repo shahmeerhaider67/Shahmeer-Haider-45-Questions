55bb0d1419e1228bed0f444929d95bb6730a016a
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:54:53 +0500

    Add files via upload

---
+++ Q39/main.js
@@ -0,0 +1,10 @@
+function cityCountry(city, country) {
+    return "".concat(city, ", ").concat(country);
+}
+// Call the function with three city-country pairs and print the returned values
+var location1 = cityCountry("Lahore", "Pakistan");
+var location2 = cityCountry("New York", "USA");
+var location3 = cityCountry("Paris", "France");
+console.log(location1);
+console.log(location2);
+console.log(location3);
