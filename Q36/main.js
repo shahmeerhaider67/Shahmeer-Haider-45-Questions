ac4def9b0b069225d0b7ae4e93e86c9292788c2e
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:50:43 +0500

    Add files via upload

---
+++ Q36/main.js
@@ -0,0 +1,5 @@
+function makeShirt(size, message) {
+    console.log("Size: ".concat(size, ", Message: \"").concat(message, "\""));
+}
+// Call the function
+makeShirt("Large", "I love TypeScript!");
