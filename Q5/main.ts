f5b9346c391ed06cdf3be390104ffbbff9300707
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 19:27:19 +0500

    Update main.ts

--- Q5/main.ts
+++ Q5/main.ts
@@ -1,1 +1,6 @@
+//#Q5
+const famous_person: string = "Albert Einstein";
+const quote: string = "Imagination is more important than knowledge.";
+const message: string = `"${quote}"\n- ${famous_person}`;
 
+console.log(message);
