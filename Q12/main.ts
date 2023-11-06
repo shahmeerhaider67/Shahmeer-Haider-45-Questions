f7ea38a924c047ca49bdf38523e03e018f5f54d2
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:13:56 +0500

    Create main.ts

---
+++ Q12/main.ts
@@ -0,0 +1,8 @@
+//#Q12
+// Define an array of friends' names
+const names: string[] = ["Batman", "Superman", "Thor", "IronMan"];
+
+// Print a personalized message to each person
+for (const name of names) {
+    console.log(`Hello ${name}, I hope you're doing well!`);
+}
