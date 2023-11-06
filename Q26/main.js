df9154bff9843ca202161ac330cc99b1c039a27f
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:37:42 +0500

    Add files via upload

---
+++ Q26/main.js
@@ -0,0 +1,14 @@
+// const alienColor: string = "green";
+// if (alienColor === "green") {
+//   console.log("Player just earned 5 points for shooting the green alien.");
+// } else {
+//   console.log("Player just earned 10 points.");
+// }
+//We are assuming that alien is not green else bloc
+var alienColor = "red";
+if (alienColor === "green") {
+    console.log("Player just earned 5 points for shooting the green alien.");
+}
+else {
+    console.log("Player just earned 10 points.");
+}
