3700b67b734f9faaf906defd1fbbd263ea315b34
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:17:08 +0500

    Add files via upload

---
+++ Q14/main.js
@@ -0,0 +1,7 @@
+// Define a list of people to invite to dinner
+var invitees = ["Christian Bale", "Tom Cruise", "Dwayne Johnson"];
+// Print invitations to each person
+for (var _i = 0, invitees_1 = invitees; _i < invitees_1.length; _i++) {
+    var invitee = invitees_1[_i];
+    console.log("Dear ".concat(invitee, ", I would like to invite you to a dinner event. Please let me know if you can make it."));
+}
