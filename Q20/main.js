c580e7f117e3262bb21921aa3ccb416db39cc78b
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:29:47 +0500

    Add files via upload

---
+++ Q20/main.js
@@ -0,0 +1,19 @@
+// Define an array of countries
+var countries = [
+    "United States",
+    "Canada",
+    "Pakistan",
+    "Australia",
+    "Germany",
+    "France",
+    "Japan",
+    "Brazil",
+    "India",
+    "South Africa"
+];
+// Print the list of countries
+console.log("List of Countries:");
+for (var _i = 0, countries_1 = countries; _i < countries_1.length; _i++) {
+    var country = countries_1[_i];
+    console.log(country);
+}
