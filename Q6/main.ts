48057d8033f0d6db551068025e7f64ece6bbfbaa
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 19:30:19 +0500

    Update main.ts

--- Q6/main.ts
+++ Q6/main.ts
@@ -1,1 +1,13 @@
+//#Q6
+// Store the person's name with whitespace characters
+const personNameWithWhitespace: string = "\t\n   John Doe   \n\t";
+
+// Print the name with whitespace
+console.log("Name with Whitespace: \"" + personNameWithWhitespace + "\"");
+
+// Strip the whitespace from the name
+const strippedName: string = personNameWithWhitespace.trim();
+
+// Print the name after stripping whitespace
+console.log("Stripped Name: \"" + strippedName + "\"");
 
