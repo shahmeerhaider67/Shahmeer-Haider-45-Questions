7d1a5caf6425661a807e50ace1be781c8ef002fb
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:47:59 +0500

    Create main.ts

---
+++ Q34/main.ts
@@ -0,0 +1,8 @@
+//#Q34
+const favoritePizzas: string[] = ["Fajita", "BBQ Tikka", "Supreme"];
+
+for (const pizza of favoritePizzas) {
+  console.log(`I like ${pizza} pizza.`);
+}
+
+console.log("Pizza is one of my favorite foods. I really love pizza!");
