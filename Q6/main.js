f65ec7af109b28d030e8764f4fdbffac0f0a210c
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 19:31:37 +0500

    Add files via upload

---
+++ Q6/main.js
@@ -0,0 +1,8 @@
+// Store the person's name with whitespace characters
+var personNameWithWhitespace = "\t\n   John Doe   \n\t";
+// Print the name with whitespace
+console.log("Name with Whitespace: \"" + personNameWithWhitespace + "\"");
+// Strip the whitespace from the name
+var strippedName = personNameWithWhitespace.trim();
+// Print the name after stripping whitespace
+console.log("Stripped Name: \"" + strippedName + "\"");
