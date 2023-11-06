dde2e9500a2a79c9bc541b79f7bbcd2c685f9630
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:50:20 +0500

    Create main.ts

---
+++ Q36/main.ts
@@ -0,0 +1,8 @@
+//#Q36
+function makeShirt(size: string, message: string): void {
+    console.log(`Size: ${size}, Message: "${message}"`);
+  }
+  
+  // Call the function
+  makeShirt("Large", "I love TypeScript!");
+  
