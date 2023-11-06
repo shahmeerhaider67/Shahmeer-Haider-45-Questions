12052105a369e24611eb8a5147557f25ab36ac2b
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 16:20:21 +0500

    Update main.ts

--- Q3/main.ts
+++ Q3/main.ts
@@ -1,1 +1,27 @@
+//#Q3
+// Store the person's name in a variable
+const personName: string = "JOHN WICK";
+
+// Print the name in lowercase
+console.log("Lowercase: " + personName.toLowerCase());
+
+// Print the name in uppercase
+console.log("Uppercase: " + personName.toUpperCase());
+
+// Convert the name to titlecase
+const titleCaseName: string = personName
+  .toLowerCase()
+  .replace(/\b\w/g, (char) => char.toUpperCase());
+
+// Print the name in titlecase
+console.log("Titlecase: " + titleCaseName);
+
+
+
+
+
+
+
+
+
 
