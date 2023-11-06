ea7db14db2139c0c66c978a66bb297714d2586cd
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:49:00 +0500

    Create main.ts

---
+++ Q35/main.ts
@@ -0,0 +1,8 @@
+//#Q35
+const animals: string[] = ["Dog", "Cat", "Rabbit"];
+
+for (const animal of animals) {
+  console.log(`A ${animal.toLowerCase()} would make a great pet.`);
+}
+
+console.log("These animals are all popular choices as pets. Any of these animals would make a great pet!");
