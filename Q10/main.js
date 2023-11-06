9f066c769685c2a48dd31a9ea71d61422fe7e3f6
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:11:10 +0500

    Add files via upload

---
+++ Q10/main.js
@@ -0,0 +1,9 @@
+// Author: [Shahmeer Haider]
+// Date: [9 September 2023]
+// Description: This program converts a temperature in Celsius to Fahrenheit.
+// Define the temperature in Celsius
+var celsiusTemperature = 25;
+// Convert Celsius to Fahrenheit
+var fahrenheitTemperature = (celsiusTemperature * 9 / 5) + 32;
+// Print the result
+console.log("".concat(celsiusTemperature, "\u00B0C is equal to ").concat(fahrenheitTemperature, "\u00B0F."));
