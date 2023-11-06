f22b6af7749efd7dd8bb5a5ff730b5a49183498f
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 22:02:31 +0500

    Add files via upload

---
+++ Q44/main.js
@@ -0,0 +1,20 @@
+function orderSandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
+    }
+    if (items.length === 0) {
+        console.log("You ordered an empty sandwich. Please add some ingredients.");
+    }
+    else {
+        console.log("You ordered a sandwich with the following items:");
+        for (var _a = 0, items_1 = items; _a < items_1.length; _a++) {
+            var item = items_1[_a];
+            console.log("- ".concat(item));
+        }
+    }
+}
+// Call the function with different numbers of arguments
+orderSandwich("Ham", "Cheese", "Lettuce");
+orderSandwich("Turkey", "Bacon");
+orderSandwich(); // Empty sandwich
