6fa3a6ec86e7fdd21e1d4821591ae34af27ef938
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:37:23 +0500

    Create main.ts

---
+++ Q26/main.ts
@@ -0,0 +1,18 @@
+//#Q26
+//We are assuming alien is green
+// const alienColor: string = "green";
+
+// if (alienColor === "green") {
+//   console.log("Player just earned 5 points for shooting the green alien.");
+// } else {
+//   console.log("Player just earned 10 points.");
+// }
+
+//We are assuming that alien is not green else block
+const alienColor: string = "red"; 
+
+if (alienColor === "green") {
+  console.log("Player just earned 5 points for shooting the green alien.");
+} else {
+  console.log("Player just earned 10 points.");
+}
