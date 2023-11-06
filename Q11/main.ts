2eaa3abb16a54eed5ca21b7b3fef54e8af35969a
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:12:13 +0500

    Create main.ts

---
+++ Q11/main.ts
@@ -0,0 +1,8 @@
+//#Q11
+// Define an array of friends' names
+const names: string[] = ["Shahmeer", "Wayne", "Shelby", "Wick"];
+
+// Print each person's name one at a time
+for (const name of names) {
+    console.log(name);
+}
