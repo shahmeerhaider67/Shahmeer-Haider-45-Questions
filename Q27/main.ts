20966ba1d1e541d2b58f71029d647178a917e017
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:38:42 +0500

    Create main.ts

---
+++ Q27/main.ts
@@ -0,0 +1,32 @@
+//#Q27
+//FOR GREEN
+const alienColor1: string = "green";
+
+if (alienColor1 === "green") {
+  console.log("Player just earned 5 points.");
+} else if (alienColor1 === "yellow") {
+  console.log("Player just earned 10 points.");
+} else if (alienColor1 === "red") {
+  console.log("Player just earned 15 points.");
+}
+const alienColor2: string = "yellow";
+
+//FOR YELLOW
+if (alienColor2 === "green") {
+  console.log("Player just earned 5 points.");
+} else if (alienColor2 === "yellow") {
+  console.log("Player just earned 10 points.");
+} else if (alienColor2 === "red") {
+  console.log("Player just earned 15 points.");
+}
+
+//FOR RED
+const alienColor3: string = "red";
+
+if (alienColor3 === "green") {
+  console.log("Player just earned 5 points.");
+} else if (alienColor3 === "yellow") {
+  console.log("Player just earned 10 points.");
+} else if (alienColor3 === "red") {
+  console.log("Player just earned 15 points.");
+}
