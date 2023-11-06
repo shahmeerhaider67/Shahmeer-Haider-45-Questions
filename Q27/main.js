c71f8f13845a72e644b65ac0293c0a1e0cce6239
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:39:09 +0500

    Add files via upload

---
+++ Q27/main.js
@@ -0,0 +1,33 @@
+//FOR GREEN
+var alienColor1 = "green";
+if (alienColor1 === "green") {
+    console.log("Player just earned 5 points.");
+}
+else if (alienColor1 === "yellow") {
+    console.log("Player just earned 10 points.");
+}
+else if (alienColor1 === "red") {
+    console.log("Player just earned 15 points.");
+}
+var alienColor2 = "yellow";
+//FOR YELLOW
+if (alienColor2 === "green") {
+    console.log("Player just earned 5 points.");
+}
+else if (alienColor2 === "yellow") {
+    console.log("Player just earned 10 points.");
+}
+else if (alienColor2 === "red") {
+    console.log("Player just earned 15 points.");
+}
+//FOR RED
+var alienColor3 = "red";
+if (alienColor3 === "green") {
+    console.log("Player just earned 5 points.");
+}
+else if (alienColor3 === "yellow") {
+    console.log("Player just earned 10 points.");
+}
+else if (alienColor3 === "red") {
+    console.log("Player just earned 15 points.");
+}
