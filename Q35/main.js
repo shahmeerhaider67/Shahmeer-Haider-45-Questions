851a697abf3edb40ca2671a157222c72d1c65d46
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:49:23 +0500

    Add files via upload

---
+++ Q35/main.js
@@ -0,0 +1,6 @@
+var animals = ["Dog", "Cat", "Rabbit"];
+for (var _i = 0, animals_1 = animals; _i < animals_1.length; _i++) {
+    var animal = animals_1[_i];
+    console.log("A ".concat(animal.toLowerCase(), " would make a great pet."));
+}
+console.log("These animals are all popular choices as pets. Any of these animals would make a great pet!");
