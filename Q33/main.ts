faf8c2f5acd1f8ed3cf730cc3aaa63441d643940
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:46:23 +0500

    Create main.ts

---
+++ Q33/main.ts
@@ -0,0 +1,18 @@
+//#Q33
+const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for (const number of numbers) {
+  let ordinalEnding: string;
+
+  if (number === 1) {
+    ordinalEnding = "st";
+  } else if (number === 2) {
+    ordinalEnding = "nd";
+  } else if (number === 3) {
+    ordinalEnding = "rd";
+  } else {
+    ordinalEnding = "th";
+  }
+
+  console.log(`${number}${ordinalEnding}`);
+}
