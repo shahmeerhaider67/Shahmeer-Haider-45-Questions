84c8ce87892566273b027939a79f257649f7265f
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:29:30 +0500

    Create main.ts

---
+++ Q20/main.ts
@@ -0,0 +1,20 @@
+//#Q20
+// Define an array of countries
+const countries: string[] = [
+    "United States",
+    "Canada",
+    "Pakistan",
+    "Australia",
+    "Germany",
+    "France",
+    "Japan",
+    "Brazil",
+    "India",
+    "South Africa"
+  ];
+  
+  // Print the list of countries
+  console.log("List of Countries:");
+  for (const country of countries) {
+    console.log(country);
+  }
