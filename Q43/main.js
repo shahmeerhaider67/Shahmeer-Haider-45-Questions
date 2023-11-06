adb1094aa546b4b312f8d49e380d2a2698036d12
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 22:00:34 +0500

    Add files via upload

---
+++ Q43/main.js
@@ -0,0 +1,30 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+//#Q43
+function showMagicians(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        console.log(magician);
+    }
+}
+function makeGreat(magicians) {
+    var greatMagicians = [];
+    for (var _i = 0, magicians_2 = magicians; _i < magicians_2.length; _i++) {
+        var magician = magicians_2[_i];
+        greatMagicians.push("".concat(magician, " the Great"));
+    }
+    return greatMagicians;
+}
+var magicianNames = ["David Copperfield", "Harry Houdini", "Penn Jillette", "Teller"];
+var greatMagicianNames = makeGreat(__spreadArray([], magicianNames, true)); // Using the spread operator to create a copy
+console.log("Original Magicians:");
+showMagicians(magicianNames);
+console.log("\nMagicians with 'the Great':");
+showMagicians(greatMagicianNames);
