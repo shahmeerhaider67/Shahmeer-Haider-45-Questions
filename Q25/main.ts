3e8d4cfb044f4538b8537c9d2ca81f1b65fa37bb
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:36:10 +0500

    Create main.ts

---
+++ Q25/main.ts
@@ -0,0 +1,14 @@
+//#Q25
+//Program for passing the text
+const alien_color = 'green';
+
+if (alien_color === 'green') {
+  console.log("Player just earned 5 points.");
+}
+
+//Program for failing the test
+const alien_color2 = 'red';
+
+if (alien_color2 === 'red') {
+  console.log("Player just earned 5 points.");
+}
