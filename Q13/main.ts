c207640f413d0b14d9a357d38b5b527e2594159e
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:15:28 +0500

    Create main.ts

---
+++ Q13/main.ts
@@ -0,0 +1,8 @@
+//#Q13
+// Define a list of modes of transportation
+const transportationModes: string[] = ["Car", "Motorcycle", "Bicycle", "Scooter"];
+
+// Print statements about these modes of transportation
+for (const mode of transportationModes) {
+    console.log(`I would like to own a ${mode}.`);
+}
