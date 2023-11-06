9c7afa35f1c4ddd47aa0b03f70c5a5f9f35b4677
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:36:28 +0500

    Add files via upload

---
+++ Q25/main.js
@@ -0,0 +1,11 @@
+//#Q25
+//Program for passing the text
+var alien_color = 'green';
+if (alien_color === 'green') {
+    console.log("Player just earned 5 points.");
+}
+//Program for failing the test
+var alien_color2 = 'red';
+if (alien_color2 === 'red') {
+    console.log("Player just earned 5 points.");
+}
