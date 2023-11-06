7775bd0724f97e8ceba25d9b6398f5ecabbff470
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:57:24 +0500

    Create main.ts

---
+++ Q41/main.ts
@@ -0,0 +1,13 @@
+//#Q41
+function showMagicians(magicians: string[]): void {
+    for (const magician of magicians) {
+      console.log(magician);
+    }
+  }
+  
+  // Create an array of magician's names
+  let magicianNames: string[] = ["David Copperfield", "Harry Houdini", "Penn Jillette", "Teller"];
+  
+  // Call the function to show the magicians
+  showMagicians(magicianNames);
+  
