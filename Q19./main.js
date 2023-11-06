04de018ef69f133d8d5fcb2ba1859648165c66ef
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:28:35 +0500

    Add files via upload

---
+++ Q19./main.js
@@ -0,0 +1,6 @@
+// Define an array of guests
+var dinnerGuests = ["LOGAN", "JACKIE CHAN", "BRUCE LEE", "IP-MAN", "WICK"];
+// Calculate the number of people invited
+var numberOfGuests = dinnerGuests.length;
+// Print a message indicating the number of people invited
+console.log("You are inviting ".concat(numberOfGuests, " people to dinner."));
