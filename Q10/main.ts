fddb7f6b72605882347e8c178ed79a4f2b002e21
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-06 21:10:31 +0500

    Create main.ts

---
+++ Q10/main.ts
@@ -0,0 +1,13 @@
+//#Q10
+// Author: [Shahmeer Haider]
+// Date: [9 September 2023]
+// Description: This program converts a temperature in Celsius to Fahrenheit.
+
+// Define the temperature in Celsius
+const celsiusTemperature: number = 25;
+
+// Convert Celsius to Fahrenheit
+const fahrenheitTemperature: number = (celsiusTemperature * 9/5) + 32;
+
+// Print the result
+console.log(`${celsiusTemperature}°C is equal to ${fahrenheitTemperature}°F.`);
