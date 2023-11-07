073be7d1b043644552d1d5bf069a68b4f72dbaea
shahmeerhaider67 <144984365+shahmeerhaider67@users.noreply.github.com>
2023-11-07 20:07:28 +0500

    Update main.ts

--- Q7/main.ts
+++ Q7/main.ts
@@ -1,1 +1,17 @@
+//#Q7
+// Addition operation
+const additionResult: number = 5 + 3;
+console.log(`Addition Result: ${additionResult}`);
+
+// Subtraction operation
+const subtractionResult: number = 12 - 4;
+console.log(`Subtraction Result: ${subtractionResult}`);
+
+// Multiplication operation
+const multiplicationResult: number = 2 * 4;
+console.log(`Multiplication Result: ${multiplicationResult}`);
+
+// Division operation
+const divisionResult: number = 64 / 8;
+console.log(`Division Result: ${divisionResult}`);
 
